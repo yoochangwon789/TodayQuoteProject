2d74a38d1d0225e27f1b22c5df388e4e0aaac8aa
CN <ckddnjs554@naver.com>
2021-08-17 16:59:59 +0900

    Feat : MainActivity Viewpager2 property 지정

--- app/src/main/java/com/yoochangwonspro/todayquoteproject/MainActivity.kt
+++ app/src/main/java/com/yoochangwonspro/todayquoteproject/MainActivity.kt
@@ -2,8 +2,14 @@ package com.yoochangwonspro.todayquoteproject
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.viewpager2.widget.ViewPager2
 
 class MainActivity : AppCompatActivity() {
+
+    private val viewPager: ViewPager2 by lazy {
+        findViewById(R.id.viewPager)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
