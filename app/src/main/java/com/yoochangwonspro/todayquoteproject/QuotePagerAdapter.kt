67ecc06ac89d30e7b662e3e1fe50a6c8b2c0141a
CN <ckddnjs554@naver.com>
2021-08-17 16:55:13 +0900

    Feat : QuotePagerAdapter onCreateViewHolder 레이아웃 인플레이터 사용 셋업

--- app/src/main/java/com/yoochangwonspro/todayquoteproject/QuotePagerAdapter.kt
+++ app/src/main/java/com/yoochangwonspro/todayquoteproject/QuotePagerAdapter.kt
@@ -1,26 +1,27 @@
 package com.yoochangwonspro.todayquoteproject
 
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 
 class QuotePagerAdapter(
-    val quote: List<Quote>
-): RecyclerView.Adapter<QuotePagerAdapter.QuoteViewHolder>() {
+    val quote: List<Quote>,
+) : RecyclerView.Adapter<QuotePagerAdapter.QuoteViewHolder>() {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): QuoteViewHolder {
-        TODO("Not yet implemented")
-    }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        QuoteViewHolder(
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.item_quote, parent, false)
+        )
 
     override fun onBindViewHolder(holder: QuoteViewHolder, position: Int) {
         TODO("Not yet implemented")
     }
 
-    override fun getItemCount(): Int {
-        TODO("Not yet implemented")
-    }
+    override fun getItemCount(): Int = quote.size
 
-    class QuoteViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+    class QuoteViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
 
     }
 }
